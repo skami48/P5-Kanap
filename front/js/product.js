90ad97314bfe6027c53c2540a6a0e97977f5814a
Sami Skouri <sami.skouri@gmail.com>
2022-12-22 14:35:27 +0100

    add servers call for index and product

---
+++ front/js/product.js
@@ -0,0 +1,48 @@
+const queryString = window.location.search;
+
+const urlParams = new URLSearchParams(queryString);
+const kanapID = urlParams.get('id');
+
+
+getKanapElements();
+function fillElements(serverValue){
+    
+    let imageElem = document.createElement("img");
+    imageElem.setAttribute("src",serverValue.imageUrl);
+    document.getElementsByClassName("item__img")[0].appendChild(imageElem);
+
+    let title = document.getElementById("title");
+    title.innerHTML = serverValue.name
+
+    let prix = document.getElementById("price");
+    prix.innerHTML = serverValue.price
+    let description = document.getElementById("description");
+    description.innerHTML = serverValue.description
+    let colors = document.getElementById("colors");
+    for (let i = 0 ; i< serverValue.colors.length;i++){
+        let color = document.createElement("option");
+        color.setAttribute("value",serverValue.colors[i]);
+        color.innerHTML = serverValue.colors[i]
+        colors.appendChild(color);
+
+    }
+
+
+}
+
+function getKanapElements(){
+    fetch("http://127.0.0.1:3000/api/products/"+kanapID).then(function(result){
+        if(result.ok){
+            return result.json();}})
+        .then(function(value){
+            console.log(value);
+            
+            fillElements(value);
+            
+        })
+        .catch(function(err){
+            console.log("connection au serveur impossible");
+        });
+
+}
+
---
+++ front/js/script.js
@@ -0,0 +1,32 @@
+function getKanapElements(){
+    fetch("http://127.0.0.1:3000/api/products/").then(function(result){
+        if(result.ok){
+            return result.json();}})
+        .then(function(value){
+            console.log(value);
+            parsekanapelements(value);
+        })
+        .catch(function(err){
+            console.log("connection au serveur impossible");
+        });
+
+}
+
+function parsekanapelements(kanaplist){
+
+
+let liste = document.getElementById("items");
+for (let i = 0 ;i < kanaplist.length;i++){
+    let element = document.createElement("a");
+    element.setAttribute("href", "./product.html?id="+kanaplist[i]._id)
+
+    liste.appendChild(element);
+    element.innerHTML= '<article><img src="'+kanaplist[i].imageUrl+'" alt="'+kanaplist[i].altTxt+'"><h3 class="productName">'+kanaplist[i].name+'</h3><p class="productDescription">'+kanaplist[i].description+'</p></article>';
+
+}
+}
+
+
+
+
+getKanapElements();

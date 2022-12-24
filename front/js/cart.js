46469826a96d3c5d2cf05342b10e30fbc8105601
Sami Skouri <sami.skouri@gmail.com>
2022-12-24 02:07:30 +0100

    better cart system

--- front/js/cart.js
+++ front/js/cart.js
@@ -14,78 +14,20 @@ function addElement(type = "div",attribute = "", attributeValue = "",innerText){
 
 
 
-function fillcartElement(elementID,localStorageElementKey, colorArray){
 
 
 
-
-
-    for(let i =0 ; i < colorArray.length;i++){
-
-        let elementNumber =  parseInt(colorArray[i]);
-;
-        if( (elementNumber!= 0 && elementNumber)){
-            let article = document.createElement("article");
-            let section = document.getElementById("cart__items");
-            section.appendChild(article)
-
-            article.setAttribute("class", "cart__item");
-            article.setAttribute("data-id", localStorageElementKey);
-            article.setAttribute("data-color", elementID.colors);
-            let divImg = article.appendChild(addElement("div","class","cart__item__img"));
-            let image = divImg.appendChild(addElement("img","src",elementID.imageUrl));
-            image.setAttribute("alt",elementID.altTxt);
-            let content = article.appendChild(addElement("div","class","cart__item__content"));
-            let contentDecription = content.appendChild(addElement("div","class","cart__item__content__description"));
-            contentDecription.appendChild(addElement("h2","","",elementID.name));
-            contentDecription.appendChild(addElement("p","","","Couleur : " + elementID.colors[i]));
-            contentDecription.appendChild(addElement("p","","","Prix : " + elementID.price+" €"));
-            let cart__item__content__settings  = content.appendChild(addElement("div","class","cart__item__content__settings"));
-            let  cart__item__content__settings__quantity =  cart__item__content__settings.appendChild(addElement("div","class","cart__item__content__settings__quantity"));
-            cart__item__content__settings__quantity.appendChild(addElement("p","","","Qté : "));
-            let cart_item_input = cart__item__content__settings__quantity.appendChild(addElement("input","type","number"));
-            cart_item_input.setAttribute("class","itemQuantity");
-            cart_item_input.setAttribute("name","itemQuantity");
-            cart_item_input.setAttribute("min","1");
-            cart_item_input.setAttribute("max","100");
-            cart_item_input.setAttribute("value",colorArray[i]);
-            let cart__item__content__settings__delete = cart__item__content__settings.appendChild(addElement("div","class", "cart__item__content__settings__delete"));
-            cart__item__content__settings__delete.appendChild(addElement("p","class","deleteItem","Supprimer"));
-        }
-   
-    }
-
-
-}
-
-
-
-function setcartlist(kanaplist){
-for (let i = 0 ; i< localStorage.length;i++){
+async function LoadLocalStorage(){
+    let storage = await localStorage.getItem("chart")
     
-    for(let x = 0;x<kanaplist.length;x++){ 
-        
-        if (kanaplist[x]._id == localStorage.key(i)){
-            console.log(localStorage.key(i)+" found");
-            let colorArray =localStorage.getItem(localStorage.key(i)); 
-            colorArray = colorArray.split(",");
-            console.log('localStorageValue is : ',colorArray )
-            fillcartElement(kanaplist[x],localStorage.key(i),colorArray);
-            
+        if( storage) {
 
-
-            
-
+            return JSON.parse(storage);
+    
+        }else{
+            return undefined;
         }
-    }
-
 }
-
-}
-
-
-
-  
 
 async function GetKanapList(){
     let kanaplist = await fetch("http://127.0.0.1:3000/api/products/")
@@ -112,83 +54,176 @@ async function GetSingleKanap(KanapID){
     return kanaplist
 }
 
-async function CalculatePrice(){
-    let kanapCartArray = document.getElementsByClassName("cart__item");
-    let totalKanapPrice = 0;
-    let totalKanap =0;
-    for (let i = 0; i < kanapCartArray.length; i++) {
-        const element = kanapCartArray[i];
-        let kanapNumber = element.getElementsByClassName("itemQuantity")[0];
-        let elementID = element.getAttribute("data-id");
-        let singleKanapElement = await GetSingleKanap(elementID)
-        console.log(parseInt(kanapNumber.getAttribute("value")) + "_nombre");
-        totalKanapPrice += parseInt(kanapNumber.getAttribute("value"))*await  singleKanapElement.price ;
-        totalKanap +=parseInt(kanapNumber.getAttribute("value"))
-    }
-    console.log(totalKanapPrice);
-    console.log(totalKanap + " kanap");
-
-    let total = document.getElementById("totalQuantity");
-    total.innerText = totalKanap;
-    let totalPrice = document.getElementById("totalPrice");
-    totalPrice.innerText = totalKanapPrice;
 
 
-}
 
-async function UpdateCart(kanap,cart__item){
-    let kanapList =await GetKanapList();
-    for (let i = 0; i < await kanapList.length; i++) {
-        const element = await kanapList[i]._id;
-        if (element == cart__item.getAttribute("data-id")){
-            console.log(kanapList[i].colors);
-            for (let index = 0; index < kanapList[i].colors.length; index++) {
-                const color = kanapList[i].colors[index];
-                let DataStoreArray = [];
-                if(cart__item.getAttribute("data-color") == color){
-                  DataStoreArray[index] =  parseInt(kanap.setAttribute("value",upDateKanapQuantity[i].value));
-                  console.log("DataStoreArray " + DataStoreArray)
-                  localStorage.setItem(element,DataStoreArray);
-                }/*else{
-                    let oldStrore = localStorage.getItem(element);
-                    console.log("oldStrore " + oldStrore);
-                    oldStrore = oldStrore.split(",")
-                    console.log("oldStrore " +oldStrore);
-                    DataStoreArray[index] = oldStrore[index];
-                }
-                localStorage.setItem(element,DataStoreArray);*/
-                
-            }
+async function fillcartElement(storage){
 
+    
+
+
+    for(let i =0 ; i < storage.id.length;i++){
+
+        let elementNumber =  storage.id[i];
+        console.log (elementNumber)
+        if( (elementNumber!='')){
+            let kanap = await GetSingleKanap(elementNumber);
+            
+            let article = document.createElement("article");
+            let section = document.getElementById("cart__items");
+            section.appendChild(article)
+            
+            article.setAttribute("class", "cart__item");
+            article.setAttribute("data-id", storage.id[i]);
+            article.setAttribute("data-color", storage.color[i]);
+            let divImg = article.appendChild(addElement("div","class","cart__item__img"));
+            let image = divImg.appendChild(addElement("img","src",kanap.imageUrl));
+            image.setAttribute("alt",kanap.altTxt);
+            let content = article.appendChild(addElement("div","class","cart__item__content"));
+            let contentDecription = content.appendChild(addElement("div","class","cart__item__content__description"));
+            contentDecription.appendChild(addElement("h2","","",kanap.name));
+            contentDecription.appendChild(addElement("p","","","Couleur : " + kanap.colors[i]));
+            contentDecription.appendChild(addElement("p","","",`Prix : ${kanap.price} €`));
+            let cart__item__content__settings  = content.appendChild(addElement("div","class","cart__item__content__settings"));
+            let  cart__item__content__settings__quantity =  cart__item__content__settings.appendChild(addElement("div","class","cart__item__content__settings__quantity"));
+            cart__item__content__settings__quantity.appendChild(addElement("p","","","Qté : "));
+            let cart_item_input = cart__item__content__settings__quantity.appendChild(addElement("input","type","number"));
+            cart_item_input.setAttribute("class","itemQuantity");
+            cart_item_input.setAttribute("name","itemQuantity");
+            cart_item_input.setAttribute("min","1");
+            cart_item_input.setAttribute("max","100");
+            cart_item_input.setAttribute("value",storage.quantity[i]);
+            let cart__item__content__settings__delete = cart__item__content__settings.appendChild(addElement("div","class", "cart__item__content__settings__delete"));
+            cart__item__content__settings__delete.appendChild(addElement("p","class","deleteItem","Supprimer"));
         }
-        
-        
+   
     }
-    CalculatePrice();
+
+
 }
+
+
 async function onEventCart(){
-    upDateKanapQuantity = document.getElementsByClassName("itemQuantity");
-    for (let i = 0; i < upDateKanapQuantity.length; i++) {
-        const element = upDateKanapQuantity[i];
-        upDateKanapQuantity[i].addEventListener('change', function(a){
+    let KanapCart = document.getElementsByClassName("cart__item");
+    
+    for (let i = 0; i < KanapCart.length; i++) {
+        const element = KanapCart[i];
+        
+        let upDateKanapQuantity = element.getElementsByClassName("itemQuantity");
+        console.log((typeof(parseInt(upDateKanapQuantity[0].value))))
+        upDateKanapQuantity[0].addEventListener('change', function(a){
             a.preventDefault();
-            console.log(upDateKanapQuantity[i].value);;
-            upDateKanapQuantity[i].setAttribute("value",upDateKanapQuantity[i].value)
-            UpdateCart(upDateKanapQuantity[i],document.getElementsByClassName("cart__item")[i]);
+            
+            upDateKanapQuantity[0].setAttribute("value",parseInt(upDateKanapQuantity[0].value))
+            if((upDateKanapQuantity[0].value == 0) || (typeof(parseInt(upDateKanapQuantity[0].value))!= 'number' )){
+                upDateKanapQuantity[0].value = 1 ;
+                upDateKanapQuantity[0].setAttribute("value",upDateKanapQuantity[0].value)
+            }
+            upDateKanapQuantity[0].value = parseInt(upDateKanapQuantity[0].value)
+            updateCart(KanapCart[i].getAttribute("data-id"),KanapCart[i].getAttribute("data-color"),parseInt(upDateKanapQuantity[0].value))
+                .then(function priceupdate(aa){
+                CalculatePrice();
+            });
+  
+        });
+
+        let DeleteButton = element.getElementsByClassName("deleteItem");
+        DeleteButton[0].addEventListener("click", function(b){
+            deleteCart(KanapCart[i].getAttribute("data-id"),KanapCart[i].getAttribute("data-color")).then(function(bb){
+                CalculatePrice();
+            });
+            element.remove();
         });
         
     } 
 }
 
 
-async function main(){
-    var  kanapList = await GetKanapList();
-    await setcartlist(kanapList);
-    await CalculatePrice();
-    await onEventCart();
-     console.log(await upDateKanapQuantity)
+async function CalculatePrice(){
+    
+    let storage = await LoadLocalStorage();
+    let totalPrice = 0;
+    let totalKanap = 0;
+    for (let i = 0; i < await storage.id.length; i++) {
+        const element = storage.id[i];
+        if (element != ""){
+            let kanap = await GetSingleKanap(element);
+            totalKanap += parseInt(storage.quantity[i]);
+            totalPrice += parseInt(kanap.price) * parseInt(storage.quantity[i]);
+
+        }
+
+        
+    }
+    
+    document.getElementById("totalQuantity").innerText = totalKanap ;
+    
+    document.getElementById("totalPrice").innerText = totalPrice;
+
+}
+async function deleteCart(kanapID, kanapColor){
+    let store = await LoadLocalStorage();
+    
+
+
+    if (store != undefined){
+        
+        
+        for (let i = 0; i <store.id.length  ; i++) {
+            const element = store.id[i];
+
+            if(store.id[i] == kanapID && kanapColor == store.color[i]){
+                store.id.splice(i,1);
+                store.color.splice(i,1);
+                store.quantity.splice(i,1);
+                localStorage.setItem("chart",JSON.stringify(store));
+ 
+                return 0
+                
+
+            }
+            
+        }
+
+    }
     
 }
+
+async function updateCart(kanapID, kanapColor, kanapNumber){
+    let store = await LoadLocalStorage();
+    
+
+
+    if (store != undefined){
+        
+        
+        for (let i = 0; i <store.id.length  ; i++) {
+            const element = store.id[i];
+
+            if(store.id[i] == kanapID && kanapColor == store.color[i]){
+                store.quantity[i] = parseInt(kanapNumber);
+                localStorage.setItem("chart",JSON.stringify(store));
+                return 0
+                
+
+            }
+            
+        }
+
+    }
+    
+}
+
+
+async function main(){
+    let storage = await LoadLocalStorage();
+    if (storage!= undefined){
+        await fillcartElement(storage);
+        await CalculatePrice();
+        await onEventCart();
+    }
+}
+
 
 
 main();
--- front/js/product.js
+++ front/js/product.js
@@ -1,99 +1,4 @@
-/*function fillElements(serverValue){ // fill the page with info
-    
-    let imageElem = document.createElement("img");
-    imageElem.setAttribute("src",serverValue.imageUrl);
-    document.getElementsByClassName("item__img")[0].appendChild(imageElem);
 
-    let title = document.getElementById("title");
-    title.innerText = serverValue.name
-
-    let prix = document.getElementById("price");
-    prix.innerText = serverValue.price
-    let description = document.getElementById("description");
-    description.innerText = serverValue.description
-    let colors = document.getElementById("colors");
-    for (let i = 0 ; i< serverValue.colors.length;i++){
-        let color = document.createElement("option");
-        color.setAttribute("value",serverValue.colors[i]);
-        color.innerText = serverValue.colors[i]
-        colors.appendChild(color);
-
-    }
-
-    kanapValue = serverValue;
-
-}
-
-function getKanapElements(){ //get the kanap info from APi
-    fetch("http://127.0.0.1:3000/api/products/"+kanapID).then(function(result){
-        if(result.ok){
-            return result.json();}})
-        .then(function(value){
-            console.log(value);
-            
-            fillElements(value);
-            
-        })
-        .catch(function(err){
-            console.log("connection au serveur impossible");
-        });
-
-}
-
-function getselectedcolor(){
-    let color = document.getElementById("colors");
-    console.log(color.value);
-    
-}
-
-
-
-
-
-
-let chart = document.getElementById("addToCart");
-chart.addEventListener("click",function(event){
-    event.preventDefault();
-
-    if(document.getElementById("colors").value != 0){
-        for (let i = 0;i<kanapValue.colors.length;i++){
-            if (document.getElementById("colors").value == kanapValue.colors[i] ){
-                if(kanapList[i]== undefined){
-                    kanapList[i] = 0;
-                }
-
-                kanapList[i] += parseInt(document.getElementById("quantity").value);
-
-        
-            }
-        }
-        
-        for( let i = 0 ; i < kanapList.length;i++){
-            if (kanapList[i] == undefined){
-                kanapList[i] = 0;
-            }
-        }
-        console.log(kanapList);
-        localStorage.setItem(kanapID,kanapList);
-    }
-
-    
-
-
-});
-
-
-
-const queryString = window.location.search;
-
-const urlParams = new URLSearchParams(queryString);
-const kanapID = urlParams.get('id'); //get kanap ID from link
-
-var kanapValue= undefined;
-
-
-let kanapList = [];
-getKanapElements(); //start */
 
 
 async function GetSingleKanap(KanapID){
@@ -138,7 +43,12 @@ function fillElements(serverValue){ // fill the page with info
 
 async function addToCartButton(kanap, kanapID){
     let store = await LoadLocalStorage();
-    console.log(store);
+    
+
+    if ( (parseInt(document.getElementById("quantity").value) == 0)||(document.getElementById("colors").value == 0)){
+        console.log("Leaving");
+        return -1;
+    }
     if (store != undefined){
         console.log("store.length "+store.id.length);
         let chosenColor = document.getElementById("colors").value;
@@ -191,9 +101,9 @@ async function LoadLocalStorage(){
 
 
 let chart= {
-    id: [""],
-    color: [""],
-    quantity : [0]
+    id: [],
+    color: [],
+    quantity : []
 
 }
 
